095bd3bcca63d021d3fb772393a6d288963f512d
lisz1012 <lisz1012@163.com>
2022-04-25 22:54:01 -0700

    bool

---
+++ my/T09_Bool.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var flag1 bool = true
+	var flag2 bool = false
+	var flag3 = 5 < 9
+	fmt.Println("Flag1 = ", flag1, " Flag2 = ", flag2, "Flag3 = ", flag3)
+}
