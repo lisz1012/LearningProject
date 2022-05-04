cab9cd85d377eab0efbbcd422af69c0501480866
lisz1012 <lisz1012@163.com>
2022-05-03 23:54:50 -0700

    添加注释

--- my/T23_Switch2.go
+++ my/T23_Switch2.go
@@ -27,7 +27,7 @@ func main() {
 	switch b := 12; { // ;不能省略，也感觉略怪异！
 	case b > 6:
 		fmt.Println("b > 6")
-		fallthrough // 只能穿透一层case！
+		fallthrough // 只能穿透一层case！go特有的规定！
 	case b > 0:
 		fmt.Println("b > 0") // 不会执行到，除非上面有fallthrough，且只会穿透一层
 	}
