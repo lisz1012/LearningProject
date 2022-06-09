b750ff5ca773f38b1e508203c276c867fd1d7a91
lisz1012 <lisz1012@163.com>
2022-06-09 02:40:16 -0700

    For循环

--- my/T24_For.go
+++ my/T24_For.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	var sum int = 0
-	for i := 1; i <= 5; i++ { // for的初始表达式只能用 := 来赋值！！
+	for i := 1; i <= 5; i++ { // for的初始表达式只能用 := 来赋值！
 		sum += i
 	}
 	println(sum)
